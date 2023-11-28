5224c158bddf07ec85ce233e2506a1fcbc5846cc
Iresha Jayasekara <jayasekarairesha@gmail.com>
2023-11-28 16:18:04 +0530

    partial-changes1

--- prototype/app/api/page.tsx
+++ prototype/app/api/page.tsx
@@ -1,8 +1,5 @@
-export  async function fetchSalesData(count:number) {
+export async function fetchSalesData(count:number) {
   const res = await fetch(`https://fakestoreapi.com/products?limit=${count}`)
-  if(!res.ok){
-    throw new Error('Failed to fetch data')
-  }
-  const products:Cloth[] = await res.json()
+  const products = await res.json()
   return products
 }
